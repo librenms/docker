040265c7e0ce7bc85d4855d646382dc5a778020b
Jonathan Senecal <JSenecal@connectitnet.com>
2018-09-14 16:52:38 -0400

    fping is in sbin

--- entrypoint.sh
+++ entrypoint.sh
@@ -143,8 +143,8 @@ cat > ${LIBRENMS_PATH}/config.d/user.php <<EOL
 EOL
 
 # Config : Fping
-echo "/usr/bin/fping -6 \$@" > /usr/bin/fping6
-chmod +x /usr/bin/fping6
+echo "/usr/sbin/fping -6 \$@" > /usr/sbin/fping6
+chmod +x /usr/sbin/fping6
 cat > ${LIBRENMS_PATH}/config.d/fping.php <<EOL
 <?php
 \$config['fping'] = "/usr/sbin/fping";
