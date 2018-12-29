750260e05f3b4444bd7d1f5eec9a55f465c51127
CrazyMax <crazy-max@users.noreply.github.com>
2018-12-29 16:06:05 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ VCS_REF=${TRAVIS_COMMIT::8}
 RUNNING_TIMEOUT=120
 RUNNING_LOG_CHECK="snmpd entered RUNNING state"
 
+PUSH_LATEST=${PUSH_LATEST:-true}
 DOCKER_USERNAME=${DOCKER_USERNAME:-librenms}
 DOCKER_REPONAME=${DOCKER_REPONAME:-librenms}
 DOCKER_LOGIN=${DOCKER_LOGIN:-librenmsbot}
@@ -31,13 +32,17 @@ elif [[ "$BRANCH" == "local" ]]; then
 fi
 
 echo "PROJECT=${PROJECT}"
+echo "VERSION=${VERSION}"
 echo "BUILD_DATE=${BUILD_DATE}"
 echo "BUILD_TAG=${BUILD_TAG}"
 echo "BUILD_WORKINGDIR=${BUILD_WORKINGDIR}"
 echo "DOCKERFILE=${DOCKERFILE}"
 echo "VCS_REF=${VCS_REF}"
+echo "PUSH_LATEST=${PUSH_LATEST}"
+echo "DOCKER_LOGIN=${DOCKER_LOGIN}"
 echo "DOCKER_USERNAME=${DOCKER_USERNAME}"
 echo "DOCKER_REPONAME=${DOCKER_REPONAME}"
+echo "QUAY_LOGIN=${QUAY_LOGIN}"
 echo "QUAY_USERNAME=${QUAY_USERNAME}"
 echo "QUAY_REPONAME=${QUAY_REPONAME}"
 echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
@@ -66,7 +71,7 @@ docker run -d --network=${PROJECT} --name ${PROJECT}-db --hostname ${PROJECT}-db
   -e "MYSQL_PASSWORD=asupersecretpassword" \
   mariadb:10.2 \
   mysqld --sql-mode= --innodb-file-per-table=1 --lower-case-table-names=0
-docker run -d --network=${PROJECT} --link ${PROJECT}-db -p 8000:80 \
+docker run -d --network=${PROJECT} --link ${PROJECT}-db \
   -e "DB_HOST=${PROJECT}-db" \
   -e "DB_NAME=librenms" \
   -e "DB_USER=librenms" \
@@ -106,19 +111,29 @@ fi
 if [[ ! -z ${DOCKER_PASSWORD} ]]; then
   echo "### Push to Docker Hub..."
   echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_LOGIN" --password-stdin > /dev/null 2>&1
-  docker tag ${BUILD_TAG} ${DOCKER_USERNAME}/${DOCKER_REPONAME}:${DOCKER_TAG}
-  docker tag ${BUILD_TAG} ${DOCKER_USERNAME}/${DOCKER_REPONAME}:${VERSION}
+  if [ "${DOCKER_TAG}" == "latest" -a "${PUSH_LATEST}" == "true" ]; then
+    docker tag ${BUILD_TAG} ${DOCKER_USERNAME}/${DOCKER_REPONAME}:${DOCKER_TAG}
+  fi
+  if [[ "${VERSION}" != "latest" ]]; then
+    docker tag ${BUILD_TAG} ${DOCKER_USERNAME}/${DOCKER_REPONAME}:${VERSION}
+  fi
   docker push ${DOCKER_USERNAME}/${DOCKER_REPONAME}
-  if [[ ! -z ${DOCKER_PASSWORD} ]]; then
+  if [[ ! -z ${MICROBADGER_HOOK} ]]; then
     echo "Call MicroBadger hook"
     curl -X POST ${MICROBADGER_HOOK}
+    echo
   fi
   echo
 fi
 if [[ ! -z ${QUAY_PASSWORD} ]]; then
   echo "### Push to Quay..."
   echo "$QUAY_PASSWORD" | docker login quay.io --username "$QUAY_LOGIN" --password-stdin > /dev/null 2>&1
-  docker tag ${BUILD_TAG} quay.io/${QUAY_USERNAME}/${QUAY_REPONAME}:${DOCKER_TAG}
-  docker tag ${BUILD_TAG} quay.io/${QUAY_USERNAME}/${QUAY_REPONAME}:${VERSION}
+  if [ "${DOCKER_TAG}" == "latest" -a "${PUSH_LATEST}" == "true" ]; then
+    docker tag ${BUILD_TAG} quay.io/${QUAY_USERNAME}/${QUAY_REPONAME}:${DOCKER_TAG}
+  fi
+  if [[ "${VERSION}" != "latest" ]]; then
+    docker tag ${BUILD_TAG} quay.io/${QUAY_USERNAME}/${QUAY_REPONAME}:${VERSION}
+  fi
   docker push quay.io/${QUAY_USERNAME}/${QUAY_REPONAME}
+  echo
 fi
