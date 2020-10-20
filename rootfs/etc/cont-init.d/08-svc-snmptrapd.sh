13da6811fbcb3aa5fd7c790407852acf63860fe1
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2020-10-20 21:36:53 +0200

    Update 08-svc-snmptrapd.sh

--- rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
+++ rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/with-contenv sh
 
 SIDECAR_SNMPTRAPD=${SIDECAR_SNMPTRAPD:-0}
-SNMPV3_USER=${SNMPV3_USER}
+SNMPV3_USER=${SNMPV3_USER:-insecureuser}
 SNMPV3_AUTH_PROTO=${SNMPV3_AUTH_PROTO:-sha}
-SNMPV3_AUTH_KEY=${SNMPV3_AUTH_KEY}
+SNMPV3_AUTH_KEY=${SNMPV3_AUTH_KEY:-insecurekey}
 SNMPV3_PRIV_PROTO=${SNMPV3_PRIV_PROTO:-aes}
-SNMPV3_PRIV_KEY=${SNMPV3_PRIV_KEY}
+SNMPV3_PRIV_KEY=${SNMPV3_PRIV_KEY:-insecurekey}
 DISABLE_SNMP_AUTHORIZATION=${DISABLE_SNMP_AUTHROZATION:-no}
 SNMP_TRAP_PROCESSING=${SNMP_TRAP_PROCESSING:-log,execute,net}
+SNMP_COMMUNITY=${SNMP_COMMUNITY:-public}
 
 # Continue only if sidecar snmptrapd container
 if [ "$SIDECAR_SNMPTRAPD" != "1" ]; then
@@ -20,15 +21,14 @@ echo ">>"
 
 if [ "$DISABLE_SNMP_AUTHORIZATION" = "yes" ]; then
   cat > /etc/snmp/snmptrapd.conf <<EOF
-  disableAuthrization $DISABLE_SNMP_AUTHORIZATION
+  disableAuthorization $DISABLE_SNMP_AUTHORIZATION
   EOF
 fi
 
 cat >> /etc/snmp/snmptrapd.conf <<EOF
 createUser $SNMPV3_USER $SNMPV3_AUTH_PROTO $SNMPV3_AUTH_KEY $SNMPV3_PRIV_PROTO $SNMPV3_PRIV_KEY
+authCommunity $SNMP_TRAP_PROCESSING $SNMP_COMMUNITY
 traphandle default /opt/librenms/snmptrap.php
-auth
-
 EOF
 
 # Create service
