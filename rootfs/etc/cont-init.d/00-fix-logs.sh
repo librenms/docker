fbe6cf70874f60628fe651e7c95e093577f5011a
CrazyMax <crazy-max@users.noreply.github.com>
2021-02-02 10:16:13 +0100

    Do not fail on permission issue

--- rootfs/etc/cont-init.d/00-fix-logs.sh
+++ rootfs/etc/cont-init.d/00-fix-logs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv sh
 
 # Fix access rights to stdout and stderr
-chown ${PUID}:${PGID} /proc/self/fd/1 /proc/self/fd/2
+chown ${PUID}:${PGID} /proc/self/fd/1 /proc/self/fd/2 || true
