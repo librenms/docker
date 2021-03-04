1cbc37d35e1c193fb6867fd1ee7ca2255ac544cc
Loïc Haas <loic.haas@haikou.ch>
2021-03-04 13:34:33 +0100

    Fix bash syntax and snmptrapd options, use stdout for logging and add authUser config

--- rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
+++ rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
@@ -20,13 +20,14 @@ echo ">> Sidecar snmptrapd container detected"
 echo ">>"
 
 if [ "$DISABLE_SNMP_AUTHORIZATION" = "yes" ]; then
-  cat > /etc/snmp/snmptrapd.conf <<EOF
-  disableAuthorization $DISABLE_SNMP_AUTHORIZATION
-  EOF
+cat > /tmp/snmptrapd.conf <<EOF
+disableAuthorization $DISABLE_SNMP_AUTHORIZATION
+EOF
 fi
 
-cat >> /etc/snmp/snmptrapd.conf <<EOF
+cat >> /tmp/snmptrapd.conf <<EOF
 createUser $SNMPV3_USER $SNMPV3_AUTH_PROTO $SNMPV3_AUTH_KEY $SNMPV3_PRIV_PROTO $SNMPV3_PRIV_KEY
+authUser $SNMP_TRAP_PROCESSING $SNMPV3_USER
 authCommunity $SNMP_TRAP_PROCESSING $SNMP_COMMUNITY
 traphandle default /opt/librenms/snmptrap.php
 EOF
@@ -36,6 +37,6 @@ mkdir -p /etc/services.d/snmptrapd
 cat > /etc/services.d/snmptrapd/run <<EOL
 #!/usr/bin/execlineb -P
 with-contenv
-/usr/sbin/snmptrapd -F
+/usr/sbin/snmptrapd -f -C -c /tmp/snmptrapd.conf -Lo
 EOL
 chmod +x /etc/services.d/snmptrapd/run
