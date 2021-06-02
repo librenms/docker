2b059898b1c81982515668ebb2715ce4482f95aa
Anton Lundin <glance@acc.umu.se>
2021-06-02 14:51:39 +0200

    Have snmptrapd listen to tcp to
    The rest of the infrastructure was setup for tcp traps to, but the
    snmptrapd listening bit was missed. This adds that.

--- rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
+++ rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
@@ -20,6 +20,6 @@ mkdir -p /etc/services.d/snmptrapd
 cat > /etc/services.d/snmptrapd/run <<EOL
 #!/usr/bin/execlineb -P
 with-contenv
-/usr/sbin/snmptrapd -f -m ALL -M /opt/librenms/mibs:/opt/librenms/mibs/cisco
+/usr/sbin/snmptrapd -f -m ALL -M /opt/librenms/mibs:/opt/librenms/mibs/cisco udp:162 tcp:162
 EOL
 chmod +x /etc/services.d/snmptrapd/run
