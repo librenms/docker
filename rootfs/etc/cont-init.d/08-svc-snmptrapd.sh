b2d4ddb2c1e3675db8b242daea726bcb5e27ef82
Anton Lundin <glance@acc.umu.se>
2021-08-01 07:26:12 +0200

    Create and enable a snmp trap handling container (#180)

---
+++ rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/with-contenv sh
+
+SIDECAR_SNNMPTRAPD=${SIDECAR_SNNMPTRAPD:-0}
+LIBRENMS_SNMP_COMMUNITY=${LIBRENMS_SNMP_COMMUNITY:-librenmsdocker}
+
+# Continue only if sidecar snmptrapd container
+if [ "$SIDECAR_SNNMPTRAPD" != "1" ]; then
+  exit 0
+fi
+
+echo ">>"
+echo ">> Sidecar snmptrapd container detected"
+echo ">>"
+
+mkdir -p /run/snmptrapd
+chown -R librenms. /run/snmptrapd
+
+sed -ie "s/@LIBRENMS_SNMP_COMMUNITY@/${LIBRENMS_SNMP_COMMUNITY}/" /etc/snmp/snmptrapd.conf
+
+# Create service
+mkdir -p /etc/services.d/snmptrapd
+cat > /etc/services.d/snmptrapd/run <<EOL
+#!/usr/bin/execlineb -P
+with-contenv
+/usr/sbin/snmptrapd -f -m ALL -M /opt/librenms/mibs:/opt/librenms/mibs/cisco udp:162 tcp:162
+EOL
+chmod +x /etc/services.d/snmptrapd/run
