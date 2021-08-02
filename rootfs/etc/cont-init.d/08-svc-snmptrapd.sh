b4a12ea10ef9fc1e54e43d4181060f1a27cc7a16
MarlinMr <51798819+MarlinMr@users.noreply.github.com>
2021-08-02 10:00:17 +0200

    Setting prefix to SNMP_

--- rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
+++ rootfs/etc/cont-init.d/08-svc-snmptrapd.sh
@@ -3,14 +3,14 @@
 SIDECAR_SNNMPTRAPD=${SIDECAR_SNNMPTRAPD:-0}
 LIBRENMS_SNMP_COMMUNITY=${LIBRENMS_SNMP_COMMUNITY:-librenmsdocker}
 SNMP_ACTION=${SNMP_ACTION:-log,execute,net}
-SNMPV3_USER=${SNMPV3_USER:-librenms_user}
-SNMPV3_AUTH=${SNMPV3_AUTH:-auth_pass}
-SNMPV3_PRIV=${SNMPV3_PRIV:-priv_pass}
-SNMPV3_AUTH_PROTO=${SNMPV3_AUTH_PROTO:-SHA}
-SNMPV3_PRIV_PROTO=${SNMPV3_PRIV_PROTO:-AES}
-SNMPV3_SECURITY_LEVEL=${SNMPV3_SECURITY_LEVEL:-priv}
+SNMP_USER=${SNMP_USER:-librenms_user}
+SNMP_AUTH=${SNMP_AUTH:-auth_pass}
+SNMP_PRIV=${SNMP_PRIV:-priv_pass}
+SNMP_AUTH_PROTO=${SNMP_AUTH_PROTO:-SHA}
+SNMP_PRIV_PROTO=${SNMP_PRIV_PROTO:-AES}
+SNMP_SECURITY_LEVEL=${SNMP_SECURITY_LEVEL:-priv}
 SNMP_ENGINEID=${SNMP_ENGINEID:-1234567890}
-DISABLE_AUTHORIZATION=${DISABLE_AUTHORIZATION:-yes}
+SNMP_DISABLE_AUTHORIZATION=${SNMP_DISABLE_AUTHORIZATION:-yes}
 
 # Continue only if sidecar snmptrapd container
 if [ "$SIDECAR_SNNMPTRAPD" != "1" ]; then
@@ -25,15 +25,15 @@ mkdir -p /run/snmptrapd
 chown -R librenms. /run/snmptrapd
 
 sed -ie "s/@LIBRENMS_SNMP_COMMUNITY@/${LIBRENMS_SNMP_COMMUNITY}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMP_ACTION@/${SNMP_ACTION}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_USER@/${SNMPV3_USER}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_AUTH@/${SNMPV3_AUTH}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_PRIV@/${SNMPV3_PRIV}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_AUTH_PROTO@/${SNMPV3_AUTH_PROTO}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_PRIV_PROTO@/${SNMPV3_PRIV_PROTO}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@SNMPV3_SECURITY_LEVEL@/${SNMPV3_SECURITY_LEVEL}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_ACTION@/${SNMP_ACTION}/" /etc/snmp/snmptrapd.conf'
+sed -ie "s/@SNMP_USER@/${SNMP_USER}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_AUTH@/${SNMP_AUTH}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_PRIV@/${SNMP_PRIV}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_AUTH_PROTO@/${SNMP_AUTH_PROTO}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_PRIV_PROTO@/${SNMP_PRIV_PROTO}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_SECURITY_LEVEL@/${SNMP_SECURITY_LEVEL}/" /etc/snmp/snmptrapd.conf
 sed -ie "s/@SNMP_ENGINEID@/${SNMP_ENGINEID}/" /etc/snmp/snmptrapd.conf
-sed -ie "s/@DISABLE_AUTHORIZATION@/${DISABLE_AUTHORIZATION}/" /etc/snmp/snmptrapd.conf
+sed -ie "s/@SNMP_DISABLE_AUTHORIZATION@/${SNMP_DISABLE_AUTHORIZATION}/" /etc/snmp/snmptrapd.conf
 
 # Create service
 mkdir -p /etc/services.d/snmptrapd
