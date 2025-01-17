gs-kamnas/librenms-docker

respect CURL_CA_BUNDLE in PHP env as well as when calling curl CLI directly

rootfs/etc/cont-init.d/03-config.sh
@@ -51,6 +51,10 @@ echo "Setting PHP INI configuration..."
 sed -i "s|memory_limit.*|memory_limit = ${MEMORY_LIMIT}|g" /etc/php82/php.ini
 sed -i "s|;date\.timezone.*|date\.timezone = ${TZ}|g" /etc/php82/php.ini
 sed -i "s|;max_input_vars.*|max_input_vars = ${MAX_INPUT_VARS}|g" /etc/php82/php.ini
+if [ -n "${CURL_CA_BUNDLE}" ]; then
+  echo "Setting PHP CA bundle path override..."
+  sed -i "s|;openssl\.cafile.*|openssl.cafile = ${CURL_CA_BUNDLE}|g" /etc/php82/php.ini
+fi
 
 # OpCache
 echo "Setting OpCache configuration..."

librenms/docker

rootfs/etc/cont-init.d/03-config.sh
@@ -70,6 +70,10 @@ echo "Setting PHP INI configuration..."
 sed -i "s|memory_limit.*|memory_limit = ${MEMORY_LIMIT}|g" /etc/php83/php.ini
 sed -i "s|;date\.timezone.*|date\.timezone = ${TZ}|g" /etc/php83/php.ini
 sed -i "s|;max_input_vars.*|max_input_vars = ${MAX_INPUT_VARS}|g" /etc/php83/php.ini
+if [ -n "${CURL_CA_BUNDLE}" ]; then
+  echo "Setting PHP CA bundle path override..."
+  sed -i "s|;openssl\.cafile.*|openssl.cafile = ${CURL_CA_BUNDLE}|g" /etc/php83/php.ini
+fi
 
 # OpCache
 echo "Setting OpCache configuration..."